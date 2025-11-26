89d0c13b6477bbf9116de046250683d7340c5148
Alex629138 <alexsephiri@gmail.com>
2025-11-26 09:08:42 +0200

    Last changes, im cooked

--- BankingSystemTest/src/main/java/com/ooadassignment/bankingsystemtest/controller/HomeController.java
+++ BankingSystemTest/src/main/java/com/ooadassignment/bankingsystemtest/controller/HomeController.java
@@ -5,13 +5,21 @@ import com.ooadassignment.bankingsystemtest.model.Investment;
 import com.ooadassignment.bankingsystemtest.model.Savings;
 import com.ooadassignment.bankingsystemtest.model.User;
 import com.ooadassignment.bankingsystemtest.session.UserSession;
+import com.ooadassignment.bankingsystemtest.util.DBConnection;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class HomeController {
 
@@ -19,10 +27,11 @@ public class HomeController {
     private Button profileButton, depositButton, withdrawButton;
 
     @FXML
-    private Label firstName, lastName, dateOfBirth, dateOfRegistration, savingsBalanceLabel, investmentBalanceLabel;
+    private Label firstName, lastName, savingsBalanceLabel, investmentBalanceLabel;
 
     private User user;
     private AccountDAO accountDAO;
+    private SavingsController savingsController;
 
 
     public HomeController(){
@@ -41,15 +50,45 @@ public class HomeController {
     private void loadUserData() {
         firstName.setText(user.getFirst_name());
         lastName.setText(user.getLast_name());
-        dateOfBirth.setText(user.getDate_of_birth().toString());
-        dateOfRegistration.setText(user.getRegistration_date().toString());
     }
 
     private void loadAccountData() {
         try {
             Savings savings = accountDAO.getSavingsAccount(user.getCustomer_id());
+
             if (savings != null) {
+
                 savingsBalanceLabel.setText(String.format("$%.2f", savings.getBalance()));
+
+            }else{
+
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("No savings account available!");
+                alert.setHeaderText("Do you want to create one now?");
+
+                Optional<ButtonType> result = alert.showAndWait();
+
+                if (result.isPresent() && result.get() == ButtonType.OK) {
+                    System.out.println("Savings Account creation clicked!");
+
+                    try{
+
+                       savingsController.createSavingsAccount(user.getCustomer_id());
+
+                        Alert savingsAlert = new Alert(Alert.AlertType.CONFIRMATION);
+
+                        savingsAlert.setTitle("Savings Account Creation");
+                        savingsAlert.setHeaderText("Savings account successfully created");
+
+                    } catch(SQLException e){
+                        e.printStackTrace();
+                    }
+
+                } else {
+
+                    System.out.println("Savings account created not clicked!.");
+
+                }
             }
             Investment investment = accountDAO.getInvestmentAccount(user.getCustomer_id());
             if (investment != null) {
@@ -89,4 +128,13 @@ public class HomeController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void backToHome() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/ooadassignment/bankingsystemtest/view/home.fxml"));
+        Scene scene = new Scene(loader.load(), 600, 400);
+        Stage stage = (Stage) depositButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
--- BankingSystemTest/src/main/java/com/ooadassignment/bankingsystemtest/controller/SavingsController.java
+++ BankingSystemTest/src/main/java/com/ooadassignment/bankingsystemtest/controller/SavingsController.java
@@ -1,7 +1,40 @@
 package com.ooadassignment.bankingsystemtest.controller;
 
+import com.ooadassignment.bankingsystemtest.model.User;
+import com.ooadassignment.bankingsystemtest.session.UserSession;
+import com.ooadassignment.bankingsystemtest.util.DBConnection;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class SavingsController {
 
+    public User user;
+
     public SavingsController(){}
+
+    public void initialize(){
+        this.user = UserSession.getInstance().getLoggedInUser();
+    }
+
+    public void createSavingsAccount(int customerId) throws SQLException {
+
+        String sql = "INSERT INTO savings (customer_id, balance, interest_rate) VALUES(?, ?, ?)";
+
+        try(Connection conn = DBConnection.getConnection()){
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, customerId);
+            stmt.setFloat(2, 0.0F);
+            stmt.setFloat(3, 7.0F);
+        }
+    }
+
+
 
 }
